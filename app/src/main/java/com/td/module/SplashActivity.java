7b4b5af454061c79d1fb3dd94172984ae833fade
TrongLuan <=>
2024-11-24 11:23:52 +0700

    ads new

--- app/src/main/java/com/td/module/SplashActivity.java
+++ app/src/main/java/com/td/module/SplashActivity.java
@@ -3,6 +3,7 @@ package com.td.module;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +18,69 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        TdAd.getInstance().loadSplashInterstitialAds(this, BuildConfig.ad_interstitial_splash, 25000, 5000, new AdCallback() {
+        /*TdAd.getInstance().loadSplashInterstitialAds(this, BuildConfig.ad_interstitial_splash, 25000, 5000, new AdCallback() {
             @Override
             public void onNextAction() {
                 super.onNextAction();
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        });
+        });*/
+
+        TdAd.getInstance().loadInterSplashPriority4SameTime(this,
+                BuildConfig.ad_interstitial_splash,
+                BuildConfig.ad_interstitial_splash,
+                BuildConfig.ad_interstitial_splash,
+                BuildConfig.ad_interstitial_splash, 30000, 5000, new AdCallback() {
+                    @Override
+                    public void onAdSplashHigh1Ready() {
+                        super.onAdSplashHigh1Ready();
+                        Log.d("LuanDev", "onAdSplashHigh1Ready: 1");
+                        TdAd.getInstance().onShowSplashPriority4(SplashActivity.this, new AdCallback() {
+                            @Override
+                            public void onNextAction() {
+                                super.onNextAction();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdSplashHigh2Ready() {
+                        super.onAdSplashHigh2Ready();
+                        Log.d("LuanDev", "onAdSplashHigh2Ready: 2");
+                        TdAd.getInstance().onShowSplashPriority4(SplashActivity.this, new AdCallback() {
+                            @Override
+                            public void onNextAction() {
+                                super.onNextAction();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdSplashHigh3Ready() {
+                        super.onAdSplashHigh3Ready();
+                        Log.d("LuanDev", "onAdSplashHigh3Ready: 3");
+                        TdAd.getInstance().onShowSplashPriority4(SplashActivity.this, new AdCallback() {
+                            @Override
+                            public void onNextAction() {
+                                super.onNextAction();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdSplashNormalReady() {
+                        super.onAdSplashNormalReady();
+                        Log.d("LuanDev", "onAdSplashNormalReady: 0");
+                        TdAd.getInstance().onShowSplashPriority4(SplashActivity.this, new AdCallback() {
+                            @Override
+                            public void onNextAction() {
+                                super.onNextAction();
+                                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        });
+                    }
+                });
     }
 }
